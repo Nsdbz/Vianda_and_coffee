5ff8824bfd1566c5980310a4064587c4ac58a030
Nsdbz <nikolasgarzon07@gmail.com>
2026-05-27 17:42:41 -0500

    fix puerto para render

--- index.js
+++ index.js
@@ -282,7 +282,7 @@ app.post('/queue', async (req, res) => {
  
 // ─── SERVIDOR ─────────────────────────────────────────────────────────────────
  
-app.listen(process.env.PORT, '127.0.0.1', () => {
+app.listen(process.env.PORT, '0.0.0.0', () => {
   console.log(`\nServidor corriendo en http://127.0.0.1:${process.env.PORT}`)
   console.log(`Admin: http://127.0.0.1:${process.env.PORT}/auth/login\n`)
 })
